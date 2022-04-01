f96455fffba257f0b20871e391779656ddf328ae
sleptsovstas <sleptsovstas.v@gmail.com>
2022-04-02 00:20:53 +0900

    create components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрдников в компанию N</h1>
+      <h2>Общее число сотрудников: </h2>
+      <h2>Приемию получат: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
