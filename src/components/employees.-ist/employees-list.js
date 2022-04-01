24b6f514234b5940e9940bd646db92bd91d87243
sleptsovstas <sleptsovstas.v@gmail.com>
2022-04-02 01:46:02 +0900

    add props

--- src/components/employees.-ist/employees-list.js
+++ src/components/employees.-ist/employees-list.js
@@ -1,13 +1,11 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
-const EmployeesList = () => {
-  return (
-    <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
-    </ul>
-  );
+const EmployeesList = ({ data }) => {
+  const elements = data.map((item) => {
+    return <EmployeesListItem {...item} />;
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
