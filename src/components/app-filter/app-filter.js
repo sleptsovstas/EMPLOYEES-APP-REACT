51a61c5267af1cf2178b4f20977400a1c13869c8
sleptsovstas <sleptsovstas.v@gmail.com>
2022-04-06 17:53:07 +0900

    rise of state

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,21 +1,22 @@
 import "./app-filter.css";
 
 const AppFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-light" type="button">
-        Все сотрудники
-      </button>
-
-      <button className="btn btn-outline-light" type="button">
-        На повышение
-      </button>
-
-      <button className="btn btn-outline-light" type="button">
-        З/П больше 1000$
-      </button>
-    </div>
-  );
-};
+    return (
+        <div className="btn-group">
+            <button type="button"
+                    className="btn btn-light">
+                    Все сотрудники
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    На повышение
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    З/П больше 1000$
+            </button>
+        </div>
+    )
+}
 
 export default AppFilter;
